915b897200357455f7b249bc2db9f8773a896c4a
Leon Klingele <leon@struktur.de>
2015-12-16 14:34:17 +0100

    Update 'Temporary Password' page text & layout

--- templates/generateTP.php
+++ templates/generateTP.php
@@ -2,26 +2,26 @@
 
 script('spreedme', 'generateTP');
 script('spreedme', 'jquery-timepicker');
+style('spreedme', 'generateTP');
 style('spreedme', 'jquery-timepicker');
 
 ?>
 
 <div>
-	<p id="info">
-		Here you can generate Temporary Passwords for users which do not have an ownCloud account in your installation.<br />
-		Just enter:<br />
-		  1. The username of the person you want to invite (this is the name which will be shown in the buddy list of spreed-webrtc)<br />
-		  2. The expiration date at which the Temporary Password ("TP") should expire<br />
-		<b>Please note</b>: Once you published a TP (to friends, family, ..) it can't be invalidated, until expiration date it reached. So carefully set the expiration date.<br /><br />
-	</p>
+	<p>Here you can generate Temporary Passwords for users which do not have an ownCloud account in your installation. Just enter:</p>
+	<ol>
+		<li>The username of the person you want to invite, e.g. "John Doe". This is the name which will be shown in the buddy list of spreed-webrtc.</li>
+		<li>The expiration date at which the Temporary Password ("TP") should expire.</li>
+	</ol>
+	<p><b>Please note</b>: Once you published a TP (to a friend, ..) it can't be invalidated, until expiration date has passed. So carefully set the expiration date.<br /><br /></p>
 	<div id="tp"></div>
 </div>
 
 <form action="#" method="POST">
 
-	Username: <input type="text" name="userid" value="" /><br />
+	Username: <input type="text" name="userid" placeholder="John Doe" value="" /><br />
 	Expiration: <input type="text" name="expiration" value="" /><br />
 
-	<br /><input type="submit" />
+	<br /><input type="submit" value="Generate" />
 
 </form>
