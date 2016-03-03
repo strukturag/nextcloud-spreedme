70fbc9379e51af5eb3a876748e47f1d758195946
Leon Klingele <leon@struktur.de>
2016-03-03 13:49:09 +0100

    Include version number in TP to automatically invalidate it after a version increase

--- security/security.php
+++ security/security.php
@@ -17,15 +17,22 @@ use OCA\SpreedME\Settings\Settings;
 
 class Security {
 
+	// TODO(leon): Rename all of the methods and variables.
+	// This got so confusing.
+
+	// Increase this with every major update of the signing process to invalidate old signatures
+	const COMBO_VERSION = 2;
+	const TP_VERSION = 2;
+
 	private function __construct() {
 
 	}
 
-	private static function getSignedUsercomboArray($userid, $key, $max_age, $expiration = 0) {
+	private static function getSignedUsercomboArray($version, $userid, $key, $max_age, $expiration = 0) {
 		if ($expiration > 0) {
-			$useridcombo = intval($expiration) . ':' . $userid;
+			$useridcombo = sprintf('%s:%s:%s', intval($expiration), $userid, $version);
 		} else {
-			$useridcombo = (time() + $max_age) . ':' . $userid;
+			$useridcombo = sprintf('%s:%s:%s', (time() + $max_age), $userid, $version);
 		}
 		$secret = base64_encode(hash_hmac('sha256', $useridcombo, $key, true));
 
@@ -36,11 +43,13 @@ class Security {
 	}
 
 	public static function getSignedCombo($userid, $expiration = 0) {
+		$version = self::COMBO_VERSION;
 		$key = Config::SPREED_WEBRTC_SHAREDSECRET;
 		$max_age = Settings::SPREED_WEBRTC_USERCOMBO_MAX_AGE;
 
 		if ($expiration > 0) {
 			return self::getSignedUsercomboArray(
+				$version,
 				$userid,
 				$key,
 				false,
@@ -48,6 +57,7 @@ class Security {
 			);
 		} else {
 			return self::getSignedUsercomboArray(
+				$version,
 				$userid,
 				$key,
 				$max_age
@@ -67,7 +77,8 @@ class Security {
 		return sprintf('%s/%s/%s', $prefix, $userid, $uniqueid);
 	}
 
-	public static function generateTemporaryPassword($userid, $expiration = 0, $forValidation = false) {
+	// TODO(leon): Refactor, this is shitty
+	public static function generateTemporaryPassword($userid, $expiration = 0, $version = self::TP_VERSION, $forValidation = false) {
 		self::requireEnabledTemporaryPassword();
 
 		// Only prevent certain characters and decorate userid if we don't want to validate a given TP
@@ -88,6 +99,7 @@ class Security {
 		if ($expiration > 0) {
 			// Use a fixed expiration date
 			$signed_combo_array = self::getSignedUsercomboArray(
+				$version,
 				$userid,
 				$key,
 				false,
@@ -96,6 +108,7 @@ class Security {
 		} else {
 			// Dynamically expire after x seconds
 			$signed_combo_array = self::getSignedUsercomboArray(
+				$version,
 				$userid,
 				$key,
 				$max_age
@@ -110,19 +123,25 @@ class Security {
 
 		$split = explode(':', $tp);
 
-		if (count($split) !== 3) {
+		// The 4 parts: expiraton, userid, version, hmac (signature of previous 3 parts)
+		if (count($split) !== 4) {
 			// Invalid tp part length
 			return false;
 		}
 
-		list($expiration, $userid, $hmac) = $split;
+		list($expiration, $userid, $version, $hmac) = $split;
 
 		if (time() > $expiration) {
 			// Expired
 			return false;
 		}
 
-		$calctp = self::generateTemporaryPassword($userid, $expiration, true); // Set forValidation flag
+		if (intval($version) !== self::TP_VERSION) {
+			// Unsupported / outdated version
+			return false;
+		}
+
+		$calctp = self::generateTemporaryPassword($userid, $expiration, $version, true); // Set forValidation flag
 		if (self::constantTimeEquals($tp, $calctp) !== true) {
 			// Incorrect hmac
 			return false;
@@ -140,7 +159,7 @@ class Security {
 
 		// TODO(leon): Do we need to split again?
 		$split = explode(':', $tp);
-		list($expiration, $userid, $hmac) = $split;
+		list($expiration, $userid, $version, $hmac) = $split;
 
 		return self::getSignedCombo($userid, $expiration);
 	}
