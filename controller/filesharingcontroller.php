59dc61fd024ff7e311dec0790cd12d305e7f5fe8
Leon Klingele <leon@struktur.de>
2017-05-11 12:00:49 +0200

    Move common validation code to its own function

--- controller/filesharingcontroller.php
+++ controller/filesharingcontroller.php
@@ -33,6 +33,18 @@ class FileSharingController extends Controller {
 		$this->rootFolder = $rootFolder;
 	}
 
+	private function validateRequest($tp) {
+		if (!Helper::isUserLoggedIn() && !Security::validateTemporaryPassword(base64_decode($tp, true))) {
+			return ErrorCodes::TEMPORARY_PASSWORD_INVALID;
+		}
+
+		if (!Helper::areFileTransferUploadsAllowed() || !Helper::doesServiceUserExist()) {
+			return ErrorCodes::FILETRANSFER_DISABLED;
+		}
+
+		return null;
+	}
+
 	/**
 	 * @NoAdminRequired
 	 * @PublicPage
@@ -41,67 +53,64 @@ class FileSharingController extends Controller {
 		$_response = array('success' => false);
 		$target = stripslashes($target); // TODO(leon): Is this really required? Found it somewhere
 
-		if (!Helper::isUserLoggedIn() && !Security::validateTemporaryPassword(base64_decode($tp, true))) {
-			$_response['error'] = ErrorCodes::TEMPORARY_PASSWORD_INVALID;
+		$err = $this->validateRequest($tp);
+		if ($err !== null) {
+			$_response['error'] = $err;
 			return new DataResponse($_response);
 		}
 
-		if (!Helper::areFileTransferUploadsAllowed() || !Helper::doesServiceUserExist()) {
-			$_response['error'] = ErrorCodes::FILETRANSFER_DISABLED;
-		} else {
-			try {
-				$file = $this->request->getUploadedFile('file');
-				if (empty($file)) {
-					throw new \Exception('No file uploaded');
-				}
-				$fileName = $file['name'];
-				if (is_array($fileName)) {
-					// TODO(leon): We should support multiple file_s_
-					throw new \Exception('Only a single file may be uploaded');
-				}
-				if ($file['error'] !== UPLOAD_ERR_OK) {
-					throw new \Exception('Upload error: ' . $file['error']);
-				}
-				// TODO(leon): We don't need this check?
-				if (!is_uploaded_file($file['tmp_name'])) {
-					throw new \Exception('Uploaded file is not an uploaded file?');
-				}
-				if ($file['size'] > Helper::getServiceUserMaxUploadSize()) {
-					throw new \Exception('Uploaded file is too big');
-				}
-				if (!Helper::hasAllowedFileExtension($fileName)) {
-					throw new \Exception('Unsupported file extension');
-				}
-
-				$serviceUserFolder = $this->rootFolder->getUserFolder(Settings::SPREEDME_SERVICEUSER_USERNAME);
-				$uploadFolder = $serviceUserFolder
-					->newFolder(Settings::SPREEDME_SERVICEUSER_UPLOADFOLDER)
-					->newFolder($target);
-				$newFile = $uploadFolder->newFile($fileName);
-				$newFile->putContent(file_get_contents($file['tmp_name']));
-
-				$shareToken = Helper::runAsServiceUser(function () use ($newFile) {
-					return \OCP\Share::shareItem(
-						'file',
-						$newFile->getId(),
-						\OCP\Share::SHARE_TYPE_LINK,
-						null, /* shareWith */
-						\OCP\Constants::PERMISSION_READ,
-						null, /* itemSourceName */
-						null/* expirationDate */
-					);
-				});
-
-				if (!is_string($shareToken)) {
-					throw new \Exception('Failed to share uploaded file');
-				}
-
-				$_response['token'] = $shareToken;
-				$_response['success'] = true;
-			} catch (\Exception $e) {
-				$this->logger->logException($e, ['app' => Settings::APP_ID]);
-				$_response['error'] = ErrorCodes::FILETRANSFER_FAILED;
+		try {
+			$file = $this->request->getUploadedFile('file');
+			if (empty($file)) {
+				throw new \Exception('No file uploaded');
 			}
+			$fileName = $file['name'];
+			if (is_array($fileName)) {
+				// TODO(leon): We should support multiple file_s_
+				throw new \Exception('Only a single file may be uploaded');
+			}
+			if ($file['error'] !== UPLOAD_ERR_OK) {
+				throw new \Exception('Upload error: ' . $file['error']);
+			}
+			// TODO(leon): We don't need this check?
+			if (!is_uploaded_file($file['tmp_name'])) {
+				throw new \Exception('Uploaded file is not an uploaded file?');
+			}
+			if ($file['size'] > Helper::getServiceUserMaxUploadSize()) {
+				throw new \Exception('Uploaded file is too big');
+			}
+			if (!Helper::hasAllowedFileExtension($fileName)) {
+				throw new \Exception('Unsupported file extension');
+			}
+
+			$serviceUserFolder = $this->rootFolder->getUserFolder(Settings::SPREEDME_SERVICEUSER_USERNAME);
+			$uploadFolder = $serviceUserFolder
+				->newFolder(Settings::SPREEDME_SERVICEUSER_UPLOADFOLDER)
+				->newFolder($target);
+			$newFile = $uploadFolder->newFile($fileName);
+			$newFile->putContent(file_get_contents($file['tmp_name']));
+
+			$shareToken = Helper::runAsServiceUser(function () use ($newFile) {
+				return \OCP\Share::shareItem(
+					'file',
+					$newFile->getId(),
+					\OCP\Share::SHARE_TYPE_LINK,
+					null, /* shareWith */
+					\OCP\Constants::PERMISSION_READ,
+					null, /* itemSourceName */
+					null/* expirationDate */
+				);
+			});
+
+			if (!is_string($shareToken)) {
+				throw new \Exception('Failed to share uploaded file');
+			}
+
+			$_response['token'] = $shareToken;
+			$_response['success'] = true;
+		} catch (\Exception $e) {
+			$this->logger->logException($e, ['app' => Settings::APP_ID]);
+			$_response['error'] = ErrorCodes::FILETRANSFER_FAILED;
 		}
 
 		return new DataResponse($_response);
@@ -115,53 +124,50 @@ class FileSharingController extends Controller {
 		$_response = array('success' => false);
 		$target = stripslashes($target); // TODO(leon): Is this really required? Found it somewhere
 
-		if (!Helper::isUserLoggedIn() && !Security::validateTemporaryPassword(base64_decode($tp, true))) {
-			$_response['error'] = ErrorCodes::TEMPORARY_PASSWORD_INVALID;
+		$err = $this->validateRequest($tp);
+		if ($err !== null) {
+			$_response['error'] = $err;
 			return new DataResponse($_response);
 		}
 
-		if (!Helper::areFileTransferUploadsAllowed() || !Helper::doesServiceUserExist()) {
-			$_response['error'] = ErrorCodes::FILETRANSFER_DISABLED;
-		} else {
-			try {
-				$serviceUserFolder = $this->rootFolder->getUserFolder(Settings::SPREEDME_SERVICEUSER_USERNAME);
-				$uploadFolder = $serviceUserFolder
-					->newFolder(Settings::SPREEDME_SERVICEUSER_UPLOADFOLDER)
-					->newFolder($target);
+		try {
+			$serviceUserFolder = $this->rootFolder->getUserFolder(Settings::SPREEDME_SERVICEUSER_USERNAME);
+			$uploadFolder = $serviceUserFolder
+				->newFolder(Settings::SPREEDME_SERVICEUSER_UPLOADFOLDER)
+				->newFolder($target);
 
-				$shares = array();
-				foreach ($uploadFolder->getDirectoryListing() as $node) {
-					if ($node->getType() !== 'file' || !Helper::hasAllowedFileExtension($node->getName())) {
-						continue;
-					}
-					$shareToken = Helper::runAsServiceUser(function () use ($node) {
-						return \OCP\Share::shareItem(
-							'file',
-							$node->getId(),
-							\OCP\Share::SHARE_TYPE_LINK,
-							null, /* shareWith */
-							\OCP\Constants::PERMISSION_READ,
-							null, /* itemSourceName */
-							null/* expirationDate */
-						);
-					});
-					$newShare = array(
-						'name' => $node->getName(),
-						'size' => $node->getSize(),
-					);
-					// Only expose token to logged-in users
-					if (Helper::isUserLoggedIn()) {
-						$newShare['token'] = $shareToken;
-					}
-					$shares[] = $newShare;
+			$shares = array();
+			foreach ($uploadFolder->getDirectoryListing() as $node) {
+				if ($node->getType() !== 'file' || !Helper::hasAllowedFileExtension($node->getName())) {
+					continue;
 				}
-
-				$_response['shares'] = $shares;
-				$_response['success'] = true;
-			} catch (\Exception $e) {
-				$this->logger->logException($e, ['app' => Settings::APP_ID]);
-				$_response['error'] = ErrorCodes::FILETRANSFER_FAILED;
+				$shareToken = Helper::runAsServiceUser(function () use ($node) {
+					return \OCP\Share::shareItem(
+						'file',
+						$node->getId(),
+						\OCP\Share::SHARE_TYPE_LINK,
+						null, /* shareWith */
+						\OCP\Constants::PERMISSION_READ,
+						null, /* itemSourceName */
+						null/* expirationDate */
+					);
+				});
+				$newShare = array(
+					'name' => $node->getName(),
+					'size' => $node->getSize(),
+				);
+				// Only expose token to logged-in users
+				if (Helper::isUserLoggedIn()) {
+					$newShare['token'] = $shareToken;
+				}
+				$shares[] = $newShare;
 			}
+
+			$_response['shares'] = $shares;
+			$_response['success'] = true;
+		} catch (\Exception $e) {
+			$this->logger->logException($e, ['app' => Settings::APP_ID]);
+			$_response['error'] = ErrorCodes::FILETRANSFER_FAILED;
 		}
 
 		return new DataResponse($_response);
